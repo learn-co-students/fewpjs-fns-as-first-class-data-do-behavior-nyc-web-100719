2d9801103b658d82e74286745048725ee89f607a
Matthew <barnabytheplumber@gmail.com>
2019-11-20 09:58:18 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  const timeInt = parseInt(time);
+  
+  if (timeInt < 12)
+  {
+    return "Good Morning";
+  }
+  else if (timeInt < 17)
+  {
+    return "Good Afternoon";
+  }
+  else 
+  {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(timeString){
+  let h1 = document.getElementById("greeting");
+  h1.innerText = timeString;
+}
